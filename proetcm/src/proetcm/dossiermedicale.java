71fdb1165d122a09c1c7207a5db0aa127062ae6b
Sarahougour <155068819+Sarahougour@users.noreply.github.com>
2024-12-23 10:31:11 +0100

    bonjour

--- proetcm/src/proetcm/dossiermedicale.java
+++
@@ -1,7 +0,0 @@
-package proetcm;
-
-public interface dossiermedicale {
-    void writeordonnance(Patient patient, String ordonnance);
-    void addobservation(Patient patient, String observation);
-    void addcertificate(Patient patient, String certificate); 
-}
